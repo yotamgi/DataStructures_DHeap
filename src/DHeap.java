c108e186efd1c1d378a45d8f2d66fa8bd7ef008c
oferprivman <oferprivman@gmail.com>
2015-07-21 17:10:40 +0300

    Mimushization of all the functions that were left.
    Also changed the function child(i,k) because it was not written as we
    were asked for- to check if the change affected HeapifyUp.
    
    IsHeap() takes O(n)- to make sure there is no better way.

--- src/DHeap.java
+++ src/DHeap.java
@@ -1,3 +1,5 @@
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+
 /**
  * D-Heap
  */
@@ -17,6 +19,7 @@ public class DHeap {
 	}
 
 	// Getter for size
+	// O(1)
 	public int getSize() {
 		return size;
 	}
@@ -27,9 +30,23 @@ public class DHeap {
 	 * The function builds a new heap from the given array. Previous data of the
 	 * heap should be erased. preconidtion: array1.length() <= max_size
 	 * postcondition: isHeap() size = array.length()
+	 * 
+	 * amortized O(n)
 	 */
-	public void arrayToHeap(DHeap_Item[] array1) {
-		return; // just for illustration - should be replaced by student code
+	public void arrayToHeap(DHeap_Item[] array1) { 
+		int n = array1.length; 
+		if (n > max_size)
+			max_size = n;		
+		size = n;
+		for (int i=0;i<n;i++){
+			array[i] = array1[i];
+			array[i].setPos(i);
+		}	
+		heapifyDown(parent(n-1));
+		for (int i=n-2;i>0;i--){
+			if (parent(i)!=parent(i+1))
+				heapifyDown(parent(i));
+		}
 	}
 
 	/**
@@ -38,10 +55,26 @@ public class DHeap {
 	 * The function returns true if and only if the D-ary tree rooted at
 	 * array[0] satisfies the heap property or size == 0.
 	 * 
+	 * O(n)
 	 */
 	public boolean isHeap() {
-		return false; // just for illustration - should be replaced by student
-						// code
+		if (size == 0)
+			return true;
+		if (array[0] == null)
+			return false;
+		if (array[0].getPos()!= 0)
+			return false;
+		for (int i=1;i<size;i++){
+			DHeap_Item tmp = array[i]; 
+			if (tmp == null)
+				return false;
+			if (tmp.getPos()!=i)
+				return false;
+			DHeap_Item parent = array[parent(i)];
+			if (parent.getKey()>tmp.getKey())
+				return false;
+		}
+		return true;
 	}
 
 	/**
@@ -52,13 +85,15 @@ public class DHeap {
 	 * The methods compute the index of the parent and the k-th child of vertex
 	 * i in a complete D-ary tree stored in an array. 1 <= k <= d. Note that
 	 * indices of arrays in Java start from 0.
+	 * 
+	 * both functions O(1)
 	 */
 	public int parent(int i) {
 		return (i-1)/d;
 	} 
 
 	public int child(int i, int k) {
-		return d*i + k + 1;
+		return d*i + k;
 	} 
 
 	/**
@@ -79,9 +114,50 @@ public class DHeap {
 	 * precondition: size > 0 isHeap()
 	 * 
 	 * postcondition: isHeap()
+	 * 
+	 * O(dlogn)
 	 */
 	public void Delete_Min() {
-		// should be replaced by student code
+		if (size == 0)
+			return;
+		switchPlaces(array, 0, size-1);
+		size--;
+		heapifyDown(0);
+	}
+
+	/**
+	 * helping function 
+	 * 
+	 * Switching the heap item in place i with it's minimum
+	 * child if needed. Then it calls itself with the child's
+	 * place as the new i.
+	 * There may be O(logn) calls.
+	 * 
+	 * 
+	 * for one recursive call - O(d)
+	 * for the entire recursion - O(dlogn) 
+	 */
+	private void heapifyDown(int i) {
+		if (size<2)
+			return;
+		int min = array[i].getKey();
+		DHeap_Item minItem = null;
+		DHeap_Item tmp;
+		for (int j=1;j<=d;j++){
+			int place = child(i,j);	
+			if (place < size){
+				tmp = array[place];
+				if (tmp.getKey()<min){
+					min = tmp.getKey();
+					minItem = tmp;
+				}
+			}
+		}
+		if (min == array[i].getKey())
+			return;
+		int place = minItem.getPos();
+		switchPlaces(array, i, place);
+		heapifyDown(place);
 	}
 
 	/**
@@ -90,9 +166,11 @@ public class DHeap {
 	 * precondition: heapsize > 0 isHeap() size > 0
 	 * 
 	 * postcondition: isHeap()
+	 * 
+	 * O(1)
 	 */
 	public DHeap_Item Get_Min() {
-		return null;// should be replaced by student code
+		return array[0];
 	}
 
 	/**
@@ -101,9 +179,15 @@ public class DHeap {
 	 * precondition: item.pos < size; item != null isHeap()
 	 * 
 	 * postcondition: isHeap()
+	 * 
+	 * O(?)
 	 */
 	public void Decrease_Key(DHeap_Item item, int delta) {
-		return;// should be replaced by student code
+		if (delta<1)
+			return;
+		int tmp = item.getKey();
+		item.setKey(tmp - delta);
+		heapifyUp(item.getPos());
 	}
 
 	/**
@@ -112,16 +196,70 @@ public class DHeap {
 	 * precondition: item.pos < size; item != null isHeap()
 	 * 
 	 * postcondition: isHeap()
+	 * 
+	 * O(dlogn)
+	 * O(?)
 	 */
 	public void Delete(DHeap_Item item) {
-		return;// should be replaced by student code
+		int place = item.getPos();
+		switchPlaces(array, place, size-1);
+		size--;
+		heapifyDown(place);
+		heapifyUp(place);
 	}
 
 	/**
 	 * Return a sorted array containing the same integers in the input array.
 	 * Sorting should be done using the DHeap.
+	 * 
+	 * O(dnlogn)
 	 */
 	public static int[] DHeapSort(int[] array) {
-		return;
+		int n = array.length;
+		int[] result = new int[n];		
+		DHeap_Item[] heapArray = new DHeap_Item[n];
+		
+		for (int i=0;i<n;i++)
+			heapArray[i] = new DHeap_Item("",array[i]);
+		
+		DHeap heap = new DHeap(2,n);
+		heap.arrayToHeap(heapArray);
+		
+		for (int i=0;i<n;i++){
+			result[i] = heap.Get_Min().getKey();
+			heap.Delete_Min();
+		}
+		return result;
 	}
+	
+	/**
+	 * switching the places of the two Heap items in the given places.
+	 * 
+	 * O(1)
+	 */
+	public void switchPlaces(DHeap_Item[] array1, int place1, int place2) {
+		DHeap_Item tmp = array1[place1];
+		array1[place1] = array1[place2];
+		array1[place2] = tmp;
+		array1[place1].setPos(place1);
+		array1[place2].setPos(place2);		
+	}
+	
+	public static void main(String[] args) {
+		DHeap hip1 = new DHeap(3, 5);
+		System.out.println(hip1.child(0,4));
+		DHeap_Item[] hip = new DHeap_Item[5];
+		hip[0] = new DHeap_Item("a",15); 
+		hip[1] = new DHeap_Item("b",10);
+		hip[2] = new DHeap_Item("c",9);
+		hip[3] = new DHeap_Item("d",4);
+		hip[4] = new DHeap_Item("e",5);
+		//for (DHeap_Item i:hip)
+		//	System.out.println(i.getName());
+		hip1.arrayToHeap(hip);
+		//for (DHeap_Item i:hip1.array)
+			//System.out.println(i.getName());
+		System.out.println(hip1.Get_Min().getName());
+	}
+	
 }
